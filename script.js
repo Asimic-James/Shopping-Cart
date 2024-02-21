2ad81a931d6a8968d0226eada0804b81c52e6dce
asimic-james <jayasimic@gmail.com>
2024-02-21 21:49:30 +0100

    features

--- script.js
+++ script.js
@@ -143,7 +143,11 @@ class ShoppingCart {
 
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
-    const tax = calculateTaxes(subTotal);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
   }
 };
 
